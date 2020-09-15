d5d3719ec85195183ca21e300002d160a154a3fa
Jansen Junior <jsd.alcantara@gmail.com>
2020-09-15 10:37:49 -0400

    :goal_net: Handle big tableHeaderData

--- src/components/CarbonaraTHead/CarbonaraTHeadData.tsx
+++ src/components/CarbonaraTHead/CarbonaraTHeadData.tsx
@@ -1,8 +1,17 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, useRef, useState, useLayoutEffect, useEffect } from 'react'
+import { useTooltip } from '../../hooks'
 import ArrowDown from '../../assets/arrowDown.svg'
 import { CarbonaraTHeadDataProps } from '../../types'
 
 export const CarbonaraTHeadData = ({ column, sortBy, onSortSelect, loading }: CarbonaraTHeadDataProps) => {
+  const [thWidth, setThWidth] = useState(0)
+  const [spanWidth, setSpanWidth] = useState(0)
+  const [formattedValue, setFormattedValue] = useState(column.label)
+  const [showTooltip, hideTooltip] = useTooltip()
+
+  const spanRef = useRef<HTMLSpanElement>(null)
+  const thRef = useRef<HTMLTableDataCellElement>(null)
+
   const style: CSSProperties = {
     minWidth: '',
     maxWidth: '',
@@ -13,6 +22,37 @@ export const CarbonaraTHeadData = ({ column, sortBy, onSortSelect, loading }: Ca
     opacity: 0,
     transform: 'translateY(-50%) rotate(0)'
   }
+
+  useLayoutEffect(() => {
+    setThWidth(thRef.current?.offsetWidth || 0)
+    setSpanWidth(spanRef.current?.offsetWidth || 0)
+  }, [thRef, spanRef])
+
+  // ? Handle overflowing string content
+  useEffect(() => {
+    let value = column.label
+    if (!!spanRef.current) {
+      // Todo: Treat cases where font-size is not in px.
+      const fontWidthPX = parseFloat(window.getComputedStyle(spanRef.current).fontSize) / 2
+      const sliceOffset = (thWidth / fontWidthPX) - (fontWidthPX * 2)
+      console.log(thWidth, spanWidth)
+
+      if (thWidth < spanWidth) {
+        value = `${value.slice(0, sliceOffset)}...`
+      }
+    }
+    setFormattedValue(value)
+  }, [thWidth, spanWidth])
+
+  // ? Add tooltip handlers
+  useEffect(() => {
+    let content = formattedValue !== column.label ? column.label : null
+
+    if (!!thRef.current && !!content && content !== null) {
+      thRef.current.addEventListener('mouseenter', () => showTooltip(content?.toUpperCase(), thRef.current))
+      thRef.current.addEventListener('mouseleave', () => hideTooltip())
+    }
+  }, [formattedValue, thRef])
 
   const handleSort = !!onSortSelect && !loading
     ? () => {
@@ -46,8 +86,8 @@ export const CarbonaraTHeadData = ({ column, sortBy, onSortSelect, loading }: Ca
   }
 
   return (
-    <th style={style} onClick={handleSort}>
-      <span>{column.label}</span>
+    <th ref={thRef} style={style} onClick={handleSort}>
+      <span ref={spanRef}>{formattedValue}</span>
       <img src={ArrowDown} style={imgStyle} />
     </th>
   )
